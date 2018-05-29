a172685b5af8b6d6ea43d66fdbfdf98ed25d4a46
Siddharth Shenoy <siddh.shenoy@gmail.com>
2018-05-29 08:58:20 +0530

    Added new string functions

--- includes/strfuncs.h
+++ includes/strfuncs.h
@@ -7,7 +7,54 @@ class strfuncs
 {
 	public:
 		static std::string* strsplit(const std::string &str,int &total,char del = ' ');
+		static std::string* strsplit(const std::string &str,int &total,std::string delimiter = " ");
+		static std::vector<int> strparts(const std::string &str,const std::string &part);
+		static std::string trim(const std::string &str);
+		static std::string replace(const std::string &str,const std::string &repstr,const std::string &sub);
+		static std::string remove(const std::string &str,const std::string &remstr);
+		static std::string toupper(const std::string &str);
+		static char toupper(const char s);
 };
+char strfuncs::toupper(const char s)
+{
+	if(s >= 'a' && s <= 'z')
+	{
+		return (s-32);
+	}
+	return s;
+}
+std::string strfuncs::toupper(const std::string &str)
+{
+	std::string result;
+	for(int i = 0; i < str.length(); i++)
+		result += toupper(str[i]);
+	return result;
+}
+std::string strfuncs::remove(const std::string &str,const std::string &remstr)
+{
+	return strfuncs::replace(str,remstr,"");
+}
+std::string strfuncs::replace(const std::string &str,const std::string &repstr,const std::string &sub)
+{
+	std::string result = "";
+	int last = 0;
+	std::vector<int> m_parts = strfuncs::strparts(str,repstr);
+	for(int i = 0; i < m_parts.size(); i++)
+	{
+		result += str.substr(last,m_parts[i]-last);
+		result += sub;
+		last = m_parts[i]+repstr.length();
+	}
+	result += str.substr(last,str.length() - last);
+	return result;
+}
+std::string strfuncs::trim(const std::string& str)
+{
+	unsigned int i = 0,j = str.length();
+	while(str[i] == ' ') i++;
+	while(str[j] == ' ') j--;
+	return str.substr(i,j-i);
+}
 std::string* strfuncs::strsplit(const std::string &str,int &total,char del)
 {
 	total = 0;
@@ -30,4 +77,54 @@ std::string* strfuncs::strsplit(const std::string &str,int &total,char del)
 	}
 	splits[i] = str.substr(last,str.length() - last);
 	return splits;
+}
+std::string* strfuncs::strsplit(const std::string &str,int &total,std::string delimiter)
+{
+	std::vector<int> locations = strfuncs::strparts(str,delimiter);
+	total = locations.size() + 1;
+	int i , last = 0;
+	std::string *splits = new std::string[total];
+	for(i = 0; i < total-1; i++)
+	{
+		splits[i] = str.substr(last,(locations[i]) - last);
+		last = locations[i] + delimiter.length();
+	}
+	splits[i] = str.substr(last,str.length() - last);
+	return splits;
+}
+std::vector<int> strfuncs::strparts(const std::string &str,const std::string &part)
+{
+	std::vector<int> partvec;
+	int i , j;
+	bool found = false;
+	for(i = 0; i < str.length(); i++)
+	{
+		if(str[i] == part[0] && part.length() == 1)
+		{
+			partvec.push_back(i);
+		}
+		else if(str[i] == part[0])
+		{	
+			if((i + part.length()) > str.length()) break;
+			for(j = 1; j < part.length(); j++)
+			{
+				if(str[i+j] == part[j])
+				{
+					found = true;					
+					continue;
+				}
+				else
+				{
+					found = false;
+					break;
+				}
+			}
+			if(found == true)
+			{
+				partvec.push_back(i);
+			}
+			i += j;
+		}	
+	}
+	return partvec;
 }
--- testmain.cpp
+++ testmain.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include "./includes/inimanager.h"
+#include "./includes/strfuncs.h"
 
 int main()
 {
-	INI mini("test.ini",true);
+//	INI mini("test.ini",true);
+	int total;
+	std::string * split = strfuncs::strsplit("PHP=>>>php,php5,php7",total,"=>>>");
+	for(int i = 0; i < total; i++)
+	{
+		std::cout << split[i] << std::endl;
+	}
+	std::string trimstring = "           This string will be trimmed           ";
+	std::cout << strfuncs::trim(trimstring) << std::endl;
+	std::string replacestring = "This string might have every 'This' in this string replaced by 'this'";
+	replacestring = strfuncs::replace(replacestring,"This","this");
+	replacestring = strfuncs::replace(replacestring,"this","This");
+	std::cout << replacestring << std::endl;	
+	replacestring = strfuncs::remove(replacestring,"This");
+	std::cout << replacestring << std::endl;
+	replacestring = strfuncs::trim(replacestring);
+	std::cout << replacestring << std::endl;
+	replacestring = strfuncs::toupper(replacestring);
+	std::cout << replacestring << std::endl;
 	return 0;
 }
