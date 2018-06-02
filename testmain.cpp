e6fa7375ff54dc3c51bfdd39b27cf02d8058dbc2
darky03 <38765096+darky03@users.noreply.github.com>
2018-06-02 18:41:58 +0530

    Update testmain.cpp

--- testmain.cpp
+++ testmain.cpp
@@ -12,42 +12,6 @@ int main()
 	inifile.WriteKey("Test","1000");
 	inifile.WriteKey("Main","1000");
 	inifile.WriteKey("Somenewkey","1000");
-	//inifile.WriteKeyBool("something",true);
 	bool val = inifile.ReadKeyBool("something");
-
-	//assert(val == false);
-	//assert(val == false);
 	return 0;
 }
-/*
-
-int total,total2;
-	std::string * split = strfuncs::strsplit("PHP=>>>php,php5,php7",total,"=>>>");
-	std::string * split2 = strfuncs::strsplit(split[1],total2,',');
-	for(int i = 0; i < total; i++)
-	{
-		std::cout << split[i] << std::endl;
-	}
-	for(int i = 0; i < total2; i++)
-	{
-		std::cout << split2[i] << std::endl;
-	}
-	std::string trimstring = "           This string will be trimmed           ";
-	std::cout << strfuncs::trim(trimstring) << std::endl;
-	std::string replacestring = "This string might have every 'This' in this string replaced by 'this'";
-	replacestring = strfuncs::replace(replacestring,"This","this");
-	replacestring = strfuncs::replace(replacestring,"this","This");
-	std::cout << replacestring << std::endl;	
-	replacestring = strfuncs::remove(replacestring,"This");
-	std::cout << replacestring << std::endl;
-	replacestring = strfuncs::trim(replacestring);
-	std::cout << replacestring << std::endl;
-	replacestring = strfuncs::toupper(replacestring);
-	std::cout << replacestring << std::endl;
-	std::cout << strfuncs::find("Test,Test,Test","Test") << std::endl;
-	AssocFile m_assoc;
-	m_assoc.SetAssocVal("=>");
-	m_assoc.OpenAssocFile("./assocfile.txt");
-	m_assoc.DisplayKeyPair();
-
-*/
